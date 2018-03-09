fd64d68981d44a0c8cf5a7fabd934e7ee2b28ebc
yacth_Mon <yacthmon@gmail.com>
2018-03-10 01:14:07 +0700

    Add getPlayerData and change mongo connect as version 3.0

--- model/database.js
+++ model/database.js
@@ -2,24 +2,24 @@ let mongo = require('mongodb').MongoClient;
 class mongodb {
     constructor(config, callback) {
         this.config = config;
-        mongo.connect("mongodb://" + config.user + ":" + config.password + "@" + config.ip + ":" + config.port + "/" + config.database + "?authMechanism=DEFAULT&authSource=" + config.database, (err, db) => {
+        mongo.connect("mongodb://" + config.user + ":" + config.password + "@" + config.ip + ":" + config.port + "/" + config.database + "?authMechanism=DEFAULT&authSource=" + config.database, (err, client) => {
             if (err) {
                 return callback ? callback(err) : err;
             }
             if (callback) callback();
-            db.close();
+            client.close();
         })
     }
 
-    async connect() {
+    async connect(db_name) {
         let _this = this;
         return new Promise((resolve, reject) => {
             if (_this.db) {
                 resolve();
             } else {
                 mongo.connect("mongodb://" + this.config.user + ":" + this.config.password + "@" + this.config.ip + ":" + this.config.port + "/" + this.config.database + "?authMechanism=DEFAULT&authSource=" + this.config.database)
-                    .then((database) => {
-                        _this.db = database;
+                    .then((client) => {
+                        _this.db = client.db(db_name);
                         resolve(true);
                     }, (err) => {
                         reject(err.message);
@@ -32,6 +32,15 @@ class mongodb {
     close() {
         this.db.close();
         // delete this;
+    }
+
+    getPlayerData(player_name) {
+        return new Promise((resolve,reject)=>{
+            this.db.collection('battleship').findOne({ player_name: player_name }, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            })
+        })
     }
 
 }
