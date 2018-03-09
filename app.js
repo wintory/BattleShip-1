17508ddab6c01009f971262a522c9319c417b733
yacth_Mon <yacthmon@gmail.com>
2018-03-10 03:45:34 +0700

    Add body-parser and export database

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ let mongodb = require('./model/database');
 let config = require('./config');
 let express = require('express');
 let app = express();
-
-app.use(require('./controllers'))
+let bodyParser = require('body-parser')
 
 let db_server = new mongodb(config.Database, (err) => {
     if (err) {
         console.log(`Cannot connect database : ${err}`);
     } else {
-        db_server.connect().then((connected) => {
+        console.log("Database connected succesful.");
+        db_server.connect(config.Database.database).then((connected) => {
             if (connected) {
+                app.use(bodyParser.json());
+                app.use(bodyParser.urlencoded({ extended: true }));
+                app.use(require('./controllers'))
                 app.listen(config.api.port, function () {
                     console.log('Battleship listen on port 3000');
                 })
             }
         });
-        console.log("Database connected succesful.");
     }
 });
+
+module.exports.db = db_server;
